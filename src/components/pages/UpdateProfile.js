4cc64a04c78d221941941c6a5b693b01b5e493d2
Rahul More <more03625@gmail.com>
2023-10-05 13:16:22 +0530

    chore: host changed

--- src/components/pages/UpdateProfile.js
+++ src/components/pages/UpdateProfile.js
@@ -6,7 +6,6 @@ import Spinner from '../layouts/Spinner';
 import ProfileSidebar from '../sections/update-profile/ProfileSidebar';
 import { profileApi } from '../../data/api/Profile/profile';
 import { Link } from '@mui/material';
-import UserImage from '../layouts/TweetCard/UserImage';
 import { twitterTokenName } from '../../helper/constant';
 
 import { Link } from '@mui/material';
